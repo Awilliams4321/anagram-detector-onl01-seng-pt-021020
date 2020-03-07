c558c135b8a29f7b4907bbbf08fc46672800a5f5
Amber Williams <awilliams1234@live.com>
2020-03-07 23:37:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,11 +7,15 @@ class Anagram
     @word = word 
   end 
   
+<<<<<<< HEAD
   def match(array_of_words)
   array_of_words.select do |element|
       (@word.split("").sort) == (element.split("").sort)
     end 
   end 
+=======
+  
+>>>>>>> 57ea3dec0ab2e01fe4663cdddd9f3a2a1f59d881
 end 
 
   
